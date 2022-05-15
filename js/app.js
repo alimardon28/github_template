7153c28ab812ecf6e3e59a3de5a97f1c6f5092e0
alimardon28 <doniyorovalimardon369@gmail.com>
2022-05-15 17:58:41 +0500

    push

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@ function sendDisplay(user) {
     card.innerHTML = `
 
    <div
-   class="box bg-darck text-light p-3 d-flex flex-row align-items-center w-100 mt-3 justify-content-between"
+   class="box bg-darck text-light p-3 d-flex flex-row align-items-center w-100  mt-3 justify-content-between"
  >
    <div class="d-flex align-items-center">
      <img src=${item.avatar_url} alt="" class="user d-block" />
@@ -68,17 +68,15 @@ async function setProfile(user) {
   userItem.forEach((item) => {
     item.addEventListener("click", async () => {
       console.log(item.dataset.id);
-
-
+      item.style.display = "none";
 
       const data = await fetch(
         `https://api.github.com/users/${item.dataset.id}`
       );
 
       const res = await data.json();
-      console.log(res);
       sendProfile(res);
-     item.classList.add('sp');
+      item.classList.add("sp");
     });
   });
 }
@@ -110,9 +108,8 @@ function sendProfile(sms) {
   personCard.innerHTML = `
 
 
-  <div class="row">
-  <div
-    class="col-4 d-flex flex-column justify-content-center align-items-center mb-3 card"
+  <div class="profile">
+  <div class="avatar d-flex flex-column justify-content-center align-items-center text-light mb-3"
   >
     <img class="img" src="${avatar_url}" alt="" />
     <h4 class="mt-5">${name}</h4>
@@ -122,8 +119,8 @@ function sendProfile(sms) {
       ${bio}
     </p>
 
-    <div class="row w-100">
-      <div class="col-6">
+    <div class="profileElement">
+      <div class="foloow">
         <i class="bi bi-people"></i>
         <span class="fw-bold">${followers}</span> Followers
       </div>
@@ -131,7 +128,7 @@ function sendProfile(sms) {
         <span class="fw-bold">${following}</span> Following
       </div>
 
-      <div class="col-12">
+      <div class="company">
         <ul class="list-unstyled">
           <li><i class="bi bi-building"></i> ${company}</li>
           <li><i class="bi bi-geo-alt"></i> ${location}</li>
@@ -143,8 +140,8 @@ function sendProfile(sms) {
     </div>
   </div>
 
-  <div class="col-8 p-3">
-    <ul class="p-2 d-flex list-unstyled">
+  <div class="repo p-3">
+    <ul class="p-2 d-flex repo__list list-unstyled">
       <li class="p-2 text-light m-1 rounded"><i class="bi bi-book"></i> Owerview</li>
       <li class="p-2 text-light m-1 rounded"><i class="bi bi-journal-bookmark"></i> Repositories <span>${public_repos}</span></li>
       <li class="p-2 text-light m-1 rounded"><i class="bi bi-kanban"></i> Projects <span>${public_gists}</span></li>
